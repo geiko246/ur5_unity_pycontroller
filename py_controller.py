207557b2036181a5ce42b50ab825165aa094b237
Tyler <geiko246@gmail.com>
2020-03-21 22:57:52 -0400

    using json msg and implementing move joint to angle method

--- py_controller.py
+++ py_controller.py
@@ -1,29 +1,57 @@
 import socket
 import time
+import json
 import keyboard
 
-host_name = socket.gethostname() 
-HOST = socket.gethostbyname(host_name)
-PORT = 11111        # Port to listen on (non-privileged ports are > 1023)
-
-print(HOST)
-
-speed = 3
-
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.connect((HOST, PORT))
-    counter = 0
+# Container for offline model of arm
+class RobotArm:
     
-    while (True):
-        joint0 = counter
-        joint1 = 0
-        joint2 = counter
-        joint3 = counter
-        joint4 = 0
-        joint5 = counter
-        toSend = str(joint0) + ' ' + str(joint1) + ' ' + str(joint2) + ' ' + str(joint3) + ' ' + str(joint4) + ' ' + str(joint5)
+    def __init__(self):
+        self.joint_angles = [0,0,0,0,0,0]
+        self.speed = 1
 
-        print(toSend)
-        s.sendall(bytes(toSend,'utf-8'))
-        counter = counter + .1
-        time.sleep(1.0/(60 * speed))
+    # joint_num - index of joint to be moved
+    # angle - angle to move towards
+    #
+    # TODO accomodate for wrapping angles
+    def move_joint_to_angle(self, joint_num, angle, cc=False):
+
+        direction = 1
+        if cc:
+            direction = -1
+
+        curr_angle = arm.joint_angles[joint_num]
+        # for now this will always move clockwise
+        
+        while (int(curr_angle) != int(angle)):
+            curr_angle = (curr_angle + .05*direction) % 360
+            self.joint_angles[joint_num] = curr_angle
+            print(bytes(json.dumps(self.__dict__),'utf-8'))
+            s.sendall(bytes(json.dumps(self.__dict__),'utf-8'))
+            time.sleep(1.0/(60 * arm.speed))
+
+if __name__ == "__main__":
+
+    # Begin script
+    host_name = socket.gethostname() 
+    HOST = socket.gethostbyname(host_name)
+    PORT = 11111        # Port to listen on (non-privileged ports are > 1023)
+
+    print(HOST)
+
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.connect((HOST, PORT))
+        counter = 0
+    
+        arm = RobotArm()
+        arm.speed = 1
+
+        arm.move_joint_to_angle(1, 20)
+        arm.move_joint_to_angle(2, 110)
+        arm.move_joint_to_angle(3, 30)
+        
+        for i in range(10):
+            arm.move_joint_to_angle(0, 10)
+            time.sleep(1)
+            arm.move_joint_to_angle(0, 350, True)
+            time.sleep(1)
